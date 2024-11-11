f00ede3eebad6f273f47a7e8794f7b90567323eb
Victor Peinado <vpeinado263@gmail.com>
2024-11-11 20:57:04 -0300

    Crear fromulario del proyecto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import CrudApp from "@/components/CrudApp";
 import Head from "next/head";
 
 export default function Home() {
@@ -9,6 +10,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <CrudApp/>
     </>
   );
 }
