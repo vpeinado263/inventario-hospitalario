f3b4c1ac8a049c63d0067bb9b34a6df194f8f3e9
Victor Peinado <vpeinado263@gmail.com>
2024-11-12 18:13:33 -0300

    Añadir modificaciones a CrudTableRow.js

--- src/components/molecules/CrudTableRow.js
+++ src/components/molecules/CrudTableRow.js
@@ -1,17 +1,24 @@
-const CrudTableRow = ({caballero, deleteData, setDataToEdit}) => {
 
-    const {name, constellation} = caballero;
  
-   return (
-   <tr>
-     <td>{name}</td>
-     <td>{constellation}</td>
-     <td>
-         <button onClick={() => setDataToEdit(caballero)}>Editar</button>
-         <button onClick={() => deleteData(caballero)}>Eliminar</button>
-     </td>
-   </tr>
-   );
- };
+   
+ 
+
+
+ const CrudTableRow = ({ insumo, deleteData, setDataToEdit}) => {
+
+  const { name, quantity} = insumo;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{quantity}</td>
+      <td>
+          <button onClick={() => setDataToEdit(insumo)}>Editar</button>
+          <button onClick={() => deleteData(insumo)}>Eliminar</button>
+      </td>
+    </tr>
+    );
+
+  };
  
  export default CrudTableRow;
