7a543a68638549c612bb285d9842210cd558a945
Victor Peinado <vpeinado263@gmail.com>
2024-11-13 19:08:20 -0300

    Insetar <th> comments

--- src/components/molecules/CrudTableRow.js
+++ src/components/molecules/CrudTableRow.js
@@ -1,12 +1,13 @@
 
  const CrudTableRow = ({ insumo, deleteData, setDataToEdit}) => {
 
-  const { name, quantity} = insumo;
+  const { name, quantity, comments} = insumo;
 
   return (
     <tr>
       <td>{name}</td>
       <td>{quantity}</td>
+      <td>{comments}</td>
       <td>
           <button onClick={() => setDataToEdit(insumo)}>Editar</button>
           <button onClick={() => deleteData(insumo)}>Eliminar</button>
