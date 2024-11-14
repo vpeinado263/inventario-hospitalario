59d57a4ee41e35dbef426b772471acc76c77a646
Victor Peinado <vpeinado263@gmail.com>
2024-11-13 21:13:17 -0300

    Incorporar fragmento a spinner y contenido principal

--- src/components/atoms/SpinnerInsumo.js
+++ src/components/atoms/SpinnerInsumo.js
@@ -2,7 +2,13 @@ import styles from "@/styles/SpinnerInsumo.module.css"
 
 const SpinnerInsumo = () => {
   return (
+    <>
+    <div className={styles.container}>
+        <h1 className={styles.title}>Sistema de Gestión </h1>
+        <h2 className={styles.description}>Inventario Hospitalario</h2>
+    </div>
     <div className={styles.spinner}></div>
+    </>
   )
 }
 
