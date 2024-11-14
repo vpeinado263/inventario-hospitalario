2e946aece268fe58688f8ff400b491c12fff12fe
Victor Peinado <vpeinado263@gmail.com>
2024-11-14 14:32:14 -0300

    Eliminar componentes de buton y contenido en archivo json

--- src/components/atoms/ResetButton.js
+++
@@ -1,8 +0,0 @@
-import Button from "./Button";
-
-
-const ResetButton = () => {
- return <Button label="Limpiar" onClick={onClick} type="reset" />;
-}
-
-export default ResetButton
--- src/components/atoms/SubmitButton.js
+++
@@ -1,7 +0,0 @@
-import Button from "./Button";
-
-const SubmitButton = () => {
- return <Button label="Enviar" onClick={onClick} type="submit" />;
-};
-
-export default SubmitButton;
