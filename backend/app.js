ffbe4d734ce4fa709b93c9d6c8e573bbc44693d4
Victor Peinado <vpeinado263@gmail.com>
2024-12-31 21:49:15 -0300

    Realizar correcciones en archivo app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const favicon = require('serve-favicon');
 const path = require('path');
 const session = require('express-session');
 const errorHandler = require('./middleware/errorHandler');
@@ -9,24 +8,30 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
+//global
 app.use(cors());
 app.use(express.json());
-
 app.use(logRequest);
-app.use(errorHandler);
 
+//Sessiones
 app.use(session({
     secret: process.env.SESSION_SECRET || 'defaultsecret',
     resave: false,
     saveUninitialized: true,
 }));
 
-app.use('/', (req, res) => {
+//Rutas
+app.use('/insumos', insumosRoutes);
+app.get('/', (req, res) => {
     res.send('Bienvenido a la API de Insumo!');
 });
 
+//Manejo de errores 404
 app.use((req, res, next) => {
     res.status(404).send('404 - Pàgina no encontrada');
 });
+
+//Errores generales
+app.use(errorHandler);
 
 module.exports = app;
