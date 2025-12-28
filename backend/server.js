a1bf23bd027528fefa06d456a13a1aee5f45a296
Victor Peinado <vpeinado263@gmail.com>
2025-12-27 22:51:50 -0300

    Agregar config final

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,7 @@
 const app = require("./app");
-const sql = require("./database/dbConnection");
 
-const startServer = async () => {
-  try {
-    await sql`SELECT 1`;
+const port = process.env.PORT || 5001;
 
-    const port = process.env.PORT || 5001;
-    app.listen(port, () => {
-      console.log(`Servidor funcionando en http://localhost:${port}`);
-    });
-  } catch (error) {
-    console.error("Error al iniciar el servidor:", error.message);
-    process.exit(1);
-  }
-};
-
-startServer();
+app.listen(port, () => {
+  console.log(`Servidor funcionando en http://localhost:${port}`);
+});
