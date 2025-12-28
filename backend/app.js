49b6cd3cfd30d78436506bedbce7a07a38ef2b78
Victor Peinado <vpeinado263@gmail.com>
2025-12-28 12:42:31 -0300

    Modificar para producción real

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "https://inventario-hospitalario.onrender.com"
+      "*",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"]
