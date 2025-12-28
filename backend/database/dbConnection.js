3b52d7595581e0c3441d88612bf8cbfc295989ed
Victor Peinado <vpeinado263@gmail.com>
2025-12-28 11:40:43 -0300

    Editar configuración dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
-);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
