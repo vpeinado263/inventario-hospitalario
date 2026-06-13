5d65b3991ec060cb007272499e3cd6c1af415f30
Victor Peinado <vpeinado263@gmail.com>
2026-06-13 00:12:33 -0300

    Configurar toaster en _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster 
+        position="top-right"
+        richColors
+        duration={3000}
+        closeButton
+      />
+    </>
+  )
 }
